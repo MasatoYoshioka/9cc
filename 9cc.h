671af37e9a378c3e9438c7ac42dd11473070d96f
yoshioka <yoshioka@bengo4.com>
2020-05-04 13:28:38 +0900

    ファイル分割

---
+++ 9cc.h
@@ -0,0 +1,61 @@
+#include <stdbool.h>
+//
+// Tokenizer
+//
+// トークンの種類
+typedef enum {
+    TK_RESERVED, // 記号
+    TK_NUM,      // 整数トークン
+    TK_EOF,      // 入力の終わりを表すトークン
+} TokenKind;
+
+typedef struct Token Token;
+
+// トークン型
+struct Token {
+    TokenKind kind;  // トークンの型
+    Token     *next; // 次の入力トークン
+    int       val;   // kindがTK_NUMの場合、その数値
+    char      *str;  // トークン文字列
+    int       len;   // トークンの長さ
+};
+
+void error(char *fmt, ...);
+void error_at(char *loc, char *fmt, ...);
+bool consume(char *op);
+void expect(char *go);
+int expect_number();
+bool at_eof();
+Token *tokenize();
+
+char *user_input;
+// 現在着目しているトークン
+Token *token;
+
+// 
+// Parser
+// 
+typedef enum {
+    ND_ADD, // +
+    ND_SUB, // -
+    ND_MUL, // *
+    ND_DIV, // /
+    ND_NUM, // 整数
+    ND_EQ,  // ==
+    ND_NE,  // !=
+    ND_LT,  // <
+    ND_LE,  // <=
+} NodeKind;
+
+typedef struct Node Node;
+
+struct Node {
+    NodeKind kind; // ノードの型
+    Node *lhs; // 左辺
+    Node *rhs; // 右辺
+    int val; // kindがND_NUMの時に使う
+};
+
+void gen(Node *node);
+
+Node *expr(void);
