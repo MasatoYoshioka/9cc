aae624bbd4a1ad744e26d19f584ff5e4c049af29
yoshioka <yoshioka@bengo4.com>
2020-05-18 07:13:24 +0900

    整数1つをコンパイルする

---
+++ test.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+assert() {
+    expected="$1"
+    input="$2"
+
+    ./9cc "$input" > tmp.s
+    cc -o tmp tmp.s
+    ./tmp
+    actual="$?"
+
+    if [ "$actual" = "$expected" ]; then
+        echo "$input => $expected"
+    else
+        echo "$input => $expected expected, but got $actual"
+        exit 1;
+    fi
+}
+
+assert 0 0
+assert 43 43
+echo OK
